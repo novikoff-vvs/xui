69c2088e671fe5e714b48bd2d8ecbc10c055df7a
Влaдислaв Новиков <v.novikov@qsoft.ru>
2025-04-06 20:31:38 +0300

    Update methods

--- dto/client.go
+++ dto/client.go
@@ -1,15 +1,17 @@
 package dto
 
+import "encoding/json"
+
 type Client struct {
-	Comment    string `json:"comment"`
-	Email      string `json:"email"`
-	Enable     bool   `json:"enable"`
-	ExpiryTime int    `json:"expiryTime"`
-	Flow       string `json:"flow"`
-	Id         string `json:"id"`
-	LimitIp    int    `json:"limitIp"`
-	Reset      int    `json:"reset"`
-	SubId      string `json:"subId"`
-	TgId       int    `json:"tgId"`
-	TotalGB    int    `json:"totalGB"`
+	Comment    string      `json:"comment"`
+	Email      string      `json:"email"`
+	Enable     bool        `json:"enable"`
+	ExpiryTime int64       `json:"expiryTime"`
+	Flow       string      `json:"flow"`
+	Id         string      `json:"id"`
+	LimitIp    int         `json:"limitIp"`
+	Reset      int         `json:"reset"`
+	SubId      string      `json:"subId"`
+	TgId       json.Number `json:"tgId"`
+	TotalGB    int64       `json:"totalGB"`
 }
